f0a492fddebe9fc40afcefd50214e20a54236cab
nuvallo <nuvallo.inc@gmail.com>
2020-03-17 20:40:34 -0400

    Static State set and user card created

--- app/src/components/layout/Navbar.js
+++ app/src/components/layout/Navbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
+  // Default props if none are passed
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github p"
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
   render() {
     return (
       <nav className="navbar bg-primary">
         <h1>
-          <i class="fab fa-github" />
-          Navbar
+          <i className={this.props.icon} />
+          {this.props.title}
         </h1>
       </nav>
     );
